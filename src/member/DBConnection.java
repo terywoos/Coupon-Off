2d501d070bb74cd74dc336ff1ba1e0c4158f9e47
literature <literature@218.38.137.28>
2017-09-20 17:08:34 +0900

    로그인

--- src/member/DBConnection.java
+++ src/member/DBConnection.java
@@ -1,13 +1,33 @@
 package member;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DBConnection {
-	private String driver = "jdbc:mysql://localhost/CooDB";
+	private String url = "jdbc:mysql://localhost/CooDB";
 	private String id = "root";
 	private String pw="1q2w3e4r!";
 	private Connection conn;
 	public void connect() {
-		System.out.println("connect");
+		try {
+			Class.forName("com.jdbc.mysql.Driver");
+			conn = DriverManager.getConnection(url,id,pw);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void disconnect() {
+		if(conn!=null) {
+			try {
+				conn.close();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public Connection getConn() {
+		return conn;
 	}
 }
--- src/member/USignUp.java
+++ src/member/USignUp.java
@@ -1,12 +1,14 @@
 package member;
 
-public class USignUp {
+import com.mysql.jdbc.Statement;
+
+public class USignUp extends DBConnection {
 	private String MId;
 	private String MPw;
 	private String MName;
 	private String MPhoneNum;
 	private String MEmail;
-	
+	private Statement stmt = null;
 	public void signUp() {
 		System.out.println(MId);
 		System.out.println(MPw);
@@ -14,6 +16,12 @@ public class USignUp {
 		System.out.println(MPhoneNum);
 		System.out.println(MEmail);
 		//제대로 다 받아오고있음
+		
+		connect();
+		System.out.println("접속된 conn객체 : " + getConn());
+		disconnect();
+		System.out.println("접속끝낸 conn객체: " + getConn());
+		
 	}
 	
 	public void setMId(String mId) {
